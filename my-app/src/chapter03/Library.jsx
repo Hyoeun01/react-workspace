00bcd86a05338ea8e6955dfe7ddd225ecd23a577
Hyoeun01 <shyoeun09@gmail.com>
2024-05-13 16:08:00 +0900

    jsx사용하지않은 컴포넌트 연습

--- my-app/src/chapter03/Library.jsx
+++ my-app/src/chapter03/Library.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Book from "./Book";
-
+import Book1 from "./Book1";
 function Library(props){
     return (
         <div>
-            <Book name="처음만난 파이썬" numOfPage={300} />
-            <Book name="처음만난 스프링" numOfPage={320} />
-            <Book name="처음만난 리액트" numOfPage={310} />
+            <Book1 name="처음만난 파이썬" numOfPage={300} />
+            <Book1 name="처음만난 스프링" numOfPage={320} />
+            <Book1 name="처음만난 리액트" numOfPage={310} />
         </div>
     );
 }

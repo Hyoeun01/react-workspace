138c9a6623b7e4ff5d3aea0be2d308962498f86e
Hyoeun01 <shyoeun09@gmail.com>
2024-05-16 12:27:29 +0900

    댓글 컴포넌트 만들기 수정

--- my-app/src/chapter05/CommentList.jsx
+++ my-app/src/chapter05/CommentList.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+    {
+        name: "사람1",
+        comment: "하이"
+    },
+    { 
+        name: "사람2",
+        comment: "하이"
+    },
+    { 
+        name: "사람3",
+        comment: "하이"
+    }
+]
 function CommentList(props){
     return (
         <div>
-            <Comment />
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />
+                );
+            })}
         </div>
     );
 }

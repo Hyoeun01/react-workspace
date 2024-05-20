d7af7da8a19817034dd0c4db04fac8d7433a12d0
Hyoeun01 <shyoeun09@gmail.com>
2024-05-20 10:06:08 +0900

    useState 이용해서 클릭하면 횟수 증가하게 하기

--- my-app/src/chapter07/Counter.jsx
+++ my-app/src/chapter07/Counter.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Counter(props){
-    var count = 0;
+//    var count = 0;
+
+    const [count, setCount]=useState(0);
 
     return (
         <div>
             <p>총 {count}번 클릭했습니다.</p>
-            <button onClick={() => count++}>클릭
+            <button onClick={() => setCount(count + 1)}>클릭
             </button>
         </div>
     );

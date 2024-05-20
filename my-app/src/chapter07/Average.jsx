2a8fec125c384058ea2eaa612c0730edb1ea707c
Hyoeun01 <shyoeun09@gmail.com>
2024-05-20 12:25:31 +0900

    useCallback 사용해보기

--- my-app/src/chapter07/Average.jsx
+++ my-app/src/chapter07/Average.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 const getAverage= numbers => {
     console.log("평균값 계산");
@@ -11,15 +11,25 @@ function Average(props){
     const [list, setList]=useState([]);
     const [number, setNumber]=useState('');
 
-    const onChange = e => {
+    const onChange = useCallback( e=>{
         setNumber(e.target.value);
-    };
+    },[]);
 
-    const onInsert= e=> {
-        const nextList = list.concat(parseInt(number));
+    const onInsert= useCallback(e => {
+        const nextList=list.concat(parseInt(number));
         setList(nextList);
         setNumber('')
-    }
+    },[number, list]);
+    
+    // const onChange = e => {
+    //     setNumber(e.target.value);
+    // };
+
+    // const onInsert= e=> {
+    //     const nextList = list.concat(parseInt(number));
+    //     setList(nextList);
+    //     setNumber('')
+    // }
     
     const avg=useMemo(()=> getAverage(list),[list]);
 

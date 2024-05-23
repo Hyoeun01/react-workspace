1b710881660074e80c46c3ec54d9e9d4948d59e2
Hyoeun01 <shyoeun09@gmail.com>
2024-05-23 12:41:17 +0900

    데이터 추가하는 버튼만들기

--- my-app/src/chapter10/InterationSample.jsx
+++ my-app/src/chapter10/InterationSample.jsx
@@ -11,8 +11,23 @@ function InterationSample(props){
     const [inputText, setInputText] = useState('');
     const [nextId, setNextId] = useState(5);
 
+    const onChange = e => setInputText(e.target.value)
+    const onClick = () => {
+        const nextNames = names.concat({
+            id : nextId , 
+            text : inputText
+        });
+        setNextId(nextId+1);
+        setNames(nextNames);
+        setInputText('');
+    }
+
     const nameList = names.map( name => <li id={name.id}>{name.text}</li>)
-    return <ul>{nameList}</ul>;
+    return <>
+    <input value = {inputText} onChange={onChange} />
+    <button onClick={onclick}>추가</button>
+    <ul>{nameList}</ul>
+    </>;
 }
 
 export default InterationSample;

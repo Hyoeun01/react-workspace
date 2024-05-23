5895458badab1808c307c4e09581eb8b96433148
Hyoeun01 <shyoeun09@gmail.com>
2024-05-23 12:30:34 +0900

    키값으로 인덱스 사용하기

--- my-app/src/chapter10/StudentItemList.jsx
+++ my-app/src/chapter10/StudentItemList.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
 const students = [
-    {id:1,name:"학생1"},
-    {id:2,name:"학생2"},
-    {id:3,name:"학생3"}
+    {//id:1,
+        name:"학생1"},
+    {//id:2,
+        name:"학생2"},
+    {//id:3,
+        name:"학생3"}
 ];
 
 function StudentItemList(props){
    return(
     <ul>
-        {students.map((student)=>{
-            return <li key={student.id}>{student.name}</li> ;
+        {students.map((student,index)=>{
+            // return <li key={student.id}>{student.name}</li> ;
+            return <li key={index}>{student.name}</li> ;
         })}
     </ul>
    );

6ad38035e15c818500edfad7d79a469ee3835939
Hyoeun01 <shyoeun09@gmail.com>
2024-05-30 16:19:35 +0900

    Todo app 만들어보기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,46 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Header from './component/Header';
+import TodoEditor from './component/TodoEditor';
+import TodoList from './component/TodoList';
+import { useRef } from 'react';
+
+const mockTodo= [
+  {id:0, isDone:false, content:"스프링공부", createdDate:new Date().getTime(),},
+  {id:1, isDone:false, content:"파이썬공부", createdDate:new Date().getTime(),},
+  {id:2, isDone:false, content:"자바공부", createdDate:new Date().getTime(),},
+]
 
 function App() {
+  const idRef = useRef(3);
+  const [todo, setTodo] = useState(mockTodo);
+  const onCreate = (content) => {
+    const newItem ={
+      id:idRef.current,
+      content,
+      isDone:false,
+      createdDate:new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  };
+
+  const onUpdate = (targetId) => {
+    setTodo(
+      todo.map((it)=> it.id ===targetId? {...it, isDone:!it.isDone} : it)
+    );
+  };
+
+  const onDelete = (targetId) =>{
+    setTodo(todo.filter((it)=> it.id !== targetId));
+  };
+
   return (
     <div className="App">
-      
+      <Header/>
+      <TodoEditor onCreate={onCreate}/>
+      <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   );
 }
---
+++ todo-app/src/component/TodoEditor.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./TodoEditor.css";
+import { useRef } from "react";
+
+const TodoEditor = ({onCreate})=>{
+    const [content, setContent] = useState("");
+    const inputRef=useRef();
+
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+    }
+    const onSubmit = () => {
+        if(!content){
+            inputRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
+    };
+    const onKeyDown = (e) => {
+        if(e.keyCode === 13) {
+            onSubmit();
+        }
+    }
+
+    return (
+        <div className="TodoEditor">
+            <h4>새로운 Todo 작성하기</h4>
+            <div className="editor_wrapper">
+                <input ref={inputRef} value={content} onChange={onChangeContent} onKeyDown={onKeyDown} placeholder="새로운 todo...." />
+                <button onClick={onSubmit}>추가</button>
+            </div>
+        </div>
+    )
+}
+
+export default TodoEditor;
